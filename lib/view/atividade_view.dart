4bca20e496b4eb23523c43a6fdeef4ede7b97f7a
lvlima <larissa.v.lima7@aluno.senai.br>
2026-04-30 13:52:08 -0300

    continuando

--- lib/view/atividade_view.dart
+++ lib/view/atividade_view.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
 
-class AtividadeView extends StatelessWidget {
+class AtividadeView extends StatefulWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Atividades')),
       body: Center(child: Text('Lista de Atividades')),
     );
+  }
+  
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    throw UnimplementedError();
   }
 }
